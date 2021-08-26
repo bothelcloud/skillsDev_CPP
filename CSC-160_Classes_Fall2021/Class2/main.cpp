70bec896f7de6fd8b9b2ea8b088aa16649a9a8ac
Jeff Bothel <16275077+JeffBothel@users.noreply.github.com>
2021-08-25 18:38:57 -0600

    Performing an in-class commit to the class code. Made overly detailed notes on this commit to illustrate ideas

---
+++ CSC-160_Classes_Fall2021/Class2/main.cpp
@@ -0,0 +1,12 @@
+// Exercise where we are starting from scratch with the program again
+
+#include <iostream>
+
+int main()
+{
+	// Utilizes the character escape that creates a new line after saying hello world
+	std::cout << "Hello world!\n";
+//  ^^^     I omitted the using statement above to instead perform the namespace reference
+
+	return 0;
+}
