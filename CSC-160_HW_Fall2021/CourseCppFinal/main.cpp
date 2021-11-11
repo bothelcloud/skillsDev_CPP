d8198987ad38fee673676369f15f8077ff0a0650
Jeff Bothel <16275077+JeffBothel@users.noreply.github.com>
2021-11-10 19:56:11 -0700

    Pushing the final project for C++ work this term to the repo

--- CSC-160_HW_Fall2021/CourseCppFinal/main.cpp
+++ CSC-160_HW_Fall2021/CourseCppFinal/main.cpp
@@ -1,44 +1,84 @@
 #include <iostream>
+#include <string>
 
 // Main program entry point
 int main()
 {
-	// Definition of the variables for the program
-	double** equations;
-	double solution[4];
-	int count = 0;
-	int i = 0, j = 0, k = 0;
+	// Declerations for the variables used in the function
+	int eqCount = 0, i = 0, j = 0, k = 0, ii = 0;
+	double** eq;
+	double* sol;
+	double factor = 0, sum = 0;
+	std::string userInput = "";
 
-	/**** Testing Case ****/
-	count = 3;
-	/**** Testing Case ****/
+	// Gathering from the user the count of equations and variables
+	std::cout << "What is the size of the array: ";
+	std::cin >> userInput;
+	eqCount = std::stod(userInput);
 
-	// Build of the array for the equations
-	equations = new double* [count];
-	for (i = 0; i <= (count - 1); i++)
+	// Building the array that will store the equations
+	eq = new double* [eqCount];
+	for (i = 0; i <= (eqCount - 1); i++)
 	{
-		equations[i] = new double[4];
+		eq[i] = new double[eqCount+1];
 	}
 
-	/**** Testing Case ****/
-	// Sample equations
-	equations[0][0] = 2.0;
-	equations[0][1] = 5.0;
-	equations[0][2] = 4.0;
-	equations[0][3] = 9.0;
-	
-	equations[1][0] = -1.0;
-	equations[1][1] = 1.0;
-	equations[1][2] = 1.0;
-	equations[1][3] = -2.0;
-	
-	equations[2][0] = 3.0;
-	equations[2][1] = 4.0;
-	equations[2][2] = -1.0;
-	equations[2][3] = 3.0;
+	// Building the solutions array
+	sol = new double[eqCount];
 
-	/**** Testing Case ****/
+	// Gathering the array inputs from the user
+	std::cout << "Input the values for the array to be assembled:\n";
+	for (i = 0; i < eqCount; i++)
+	{
+		for (ii = 0; ii < (eqCount + 1); ii++)
+		{
+			std::cout << "a[" << (i + 1) << "][" << (ii + 1) << "]: ";
+			std::cin >> userInput;
+			eq[i][ii] = std::stod(userInput);
+		}
+	}
+	std::cout << "\n";
 
-	// Gaussian elimination processing
-	
+	// Gaussian elimination
+	for (i = 0; i < eqCount; i++)
+	{
+		for (j = i + 1; j < eqCount; j++)
+		{
+			factor = -eq[j][i] / eq[i][i];
+			for (k = i; k < (eqCount + 1); k++)
+			{
+				eq[j][k] = eq[j][k] + factor * eq[i][k];
+			}
+		}
+	}
+
+	// Backwards substitution
+	sol[(eqCount - 1)] = eq[(eqCount - 1)][eqCount] / eq[(eqCount - 1)][(eqCount - 1)];
+	for (i = eqCount - 1; i >= 0; i--)
+	{
+		sum = 0;
+		for (j = i + 1; j < eqCount; j++)
+		{
+			sum = sum + eq[i][j] * sol[j];
+		}
+		sol[i] = (eq[i][eqCount] - sum) / eq[i][i];
+	}
+
+	// Output of the solution to the user
+	std::cout << "The solution for the system of euqations is ";
+	for (i = 0; i < eqCount; i++)
+	{
+		std::cout << "x[" << (i + 1) << "]=" << sol[i];
+		if ((i + 1) < eqCount)
+		{
+			std::cout << ", ";
+		}
+		else
+		{
+			std::cout << "\n";
+		}
+	}
+
+	// Graceful exit of the program
+	return 0;
 }
